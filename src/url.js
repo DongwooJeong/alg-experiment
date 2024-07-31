924ee36541bc229e0171d62fc9b0eb3b4a2acf45
Dongwoo Jeong <dongwoo.jeong@outlook.com>
2024-08-01 06:33:33 +0900

    change url

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
 // export const MY_URL = "https://backend-424707.du.r.appspot.com";
 
-export const MY_URL = "http://localhost:3001";
-// export const MY_URL = "https://algorithm-experiment-c09a8ea350bf.herokuapp.com";
+// export const MY_URL = "http://localhost:3001";
+export const MY_URL = "https://algorithm-experiment-c09a8ea350bf.herokuapp.com";
